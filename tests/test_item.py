daf20285bf06b12e5b94f2df65c90b438e491f90
rtangels <VanshinChaineezMaster@yandex.ru>
2023-04-16 12:44:12 +0300

    Hw13_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+def test_calculate_total_price():
+    product = Item('Брюква', 2.5, 6)
+    assert product.calculate_total_price() == 15.0
+
+def test_apply_discount():
+    product = Item('Брюква', 2.5, 6)
+    product.pay_rate = 0.8
+    product.apply_discount()
+    assert product.price == 2
